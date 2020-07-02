07571e8b885de20bdf5d5b09b435bc0436f27d46
Jessica Perez <1jessicaperez@gmail.com>
2020-07-02 18:02:51 -0400

    Pseudo code updated, style added and README created

---
+++ README.md
@@ -0,0 +1,36 @@
+## Purpose of this project
+
+Create an application that generates a random password based on user input that will meet certain creteria.
+
+If you are unfamiliar with special characters, take a look at [some examples](https://www.owasp.org/index.php/Password_special_characters).
+
+## What I did
+
+```
+I included pseudocode in the JS file to facilitate understanding
+Generate button is active and working
+Web app is responsive
+User creteria HAS to meet web app creterias
+Web app creteria are the following:
+- password has to be between 8 and 128 characters long
+- password HAS to include at least 1 number, 1 lower case letter, 1 cap letter, and 1 symbol.
+Once user's input meets ALL creteria, it will generate a strong and secure password
+The new password will appear in place of "Your secure password" on our web page
+```
+
+## Web app screenshot
+
+![web page screenshot]()
+
+## File submission
+
+HTML and CSS files submitted for review:
+
+- The URL of the deployed application:
+  ![Deployed app URL]()
+
+- The URL of the GitHub repository:
+
+![GitHub repository]()
+
+© 2019 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.
--- script.js
+++ script.js
@@ -10,44 +10,49 @@ var alphabetLowercase = "abcdefghijklmnopqrstuvwxyz".split("");
 var alphabetCaps = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 var num = "1234567890".split("");
 var sym = "!#$ %& '()*+,-./:;<=>?@[]^_`{|}~".split("");
+
 // variable grouping all characters into 1 string then convert into an array
 var majorBank = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!#$ %& '()*+,-./:;<=>?@[]^_`{|}~".split(
   ""
 );
-//set a newPassword variable
+//set a newPassword variable to an empty array we will populate as we get user input
 var newPassword = [];
 
-// Write password to the #password input
+// Write password to the #password input with generatePassword() function
+
 function generatePassword() {
   //declare passwordLength var inside the generatePassword function
   var passwordLength;
   //ask user how long they want the password to be
   passwordLength = prompt("How long would you like the password to be?");
-  //if the password>128 or <8, run the function generatePassword again, else, move on
+  //if the password >128 or <8, user does NOT meet creteria, run the function generatePassword again, if user input meets creteria, move on to next
   if (passwordLength > 128 || passwordLength < 8) {
     alert("Please choose a valid number");
     return generatePassword();
   }
-  //vars set to false inside function so that they reset each time we run the function
+  // character variables set to false inside the function so that they reset each time we hit generate button
   var numbers = false;
   var symbols = false;
   var caps = false;
   var lowercase = false;
   var confirms = 0;
-  //create an empty array for the newpassword, where new characters will populate
+
+  //set newPassword variable to an empty array, where user input will live
   var newPassword = [];
 
   // check if user wants these character types in their password, will change value to true
-  numbers = confirm("Do you want numbers in your password?");
+  numbers = confirm("Do you want numbers in your password - enter at least 1?");
   console.log(numbers);
-  symbols = confirm("Do you want symbols in your password?");
+  symbols = confirm("Do you want symbols in your password - enter at least 1?");
   console.log(symbols);
-  caps = confirm("Do you want caps in your password?");
+  caps = confirm("Do you want caps in your password - enter at least 1?");
   console.log(caps);
-  lowercase = confirm("Do you want numbers in your password?");
+  lowercase = confirm(
+    "Do you want lowercases in your password - enter at least 1?"
+  );
   console.log(lowercase);
 
-  // count the number of confirms and get the first 4 characters of newPassword
+  // count the number of confirms and get the first 4 characters of newPassword, these need to include at least 1 of each character type
   if (numbers) {
     confirms = confirms + 1;
   }
@@ -60,9 +65,14 @@ function generatePassword() {
   if (lowercase) {
     confirms = confirms + 1;
   }
+  //user needs to have at lease 1 of each data type - if not, return to the beginning
+  if (confirms < 4) {
+    alert("Choose at least 1 of each data type. Try again!");
+    return generatePassword();
+  }
   console.log(confirms);
 
-  // if confirmed, pull randomly at least 1 character from each of the confirmed lists and add to the newPassword
+  // if confirmed, pull randomly at least 1 character from each of the confirmed lists and add the character at the end of the newPassword array
   if (numbers) {
     newPassword.push(num[randomPull(num)]);
   }
@@ -77,11 +87,11 @@ function generatePassword() {
   }
   console.log(newPassword);
 
-  //determine how many characters to pull from majorBank, and add at the beginning of newPassword array
+  //now that we have at least 1 of each character type, determine how many characters to pull from majorBank, and add at the beginning of newPassword array
   for (var i = 0; i < passwordLength - confirms; i++) {
     newPassword.unshift(majorBank[randomPull(majorBank)]);
   }
-  //create a newpassword array
+  //create a newpassword array by joining all characters
   console.log(newPassword);
   newPassword = newPassword.join("");
   password = newPassword;
